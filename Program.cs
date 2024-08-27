f905464c18d0df1479fb105fdae6867cb181a88f
FrancoRugg <francomarceloruggiero@gmail.com>
2024-08-27 11:43:58 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using PruebaRedis;
+
+RedisHandler redisHandler = new RedisHandler();
+
+redisHandler.Set("Prueba 2", "Esto es una prueba");
+
+string result = redisHandler.Get("Prueba 2");
+
+Console.WriteLine(result);
